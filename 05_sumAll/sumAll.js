d41db67942d0b42ebb4f33452ad13477aa713f0a
TlonUqbar <luismadrigal@duck.com>
2023-07-02 16:21:10 -0700

    feat: Add code to make tests pass for exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+  let array = [];
+  start = args[0];
+  end = args[1];
+  
+  if (start < 0 || end <  0 ) return "ERROR";
+  if (typeof(start) !== "number" || typeof(end) !== "number") return "ERROR";
+  if (start > end) {
+    start = args[1];
+    end = args[0];
+  }
 
+  for(let i = start; i <= end; i++) {
+    array.push(i);
+  }
+
+  return array.reduce( (accumulator, current) => accumulator + current, 0);
+  
 };
 
 // Do not edit below this line
