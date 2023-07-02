0702c3e81095f99a2bd2e49a28f19550fc19f1cf
TlonUqbar <luismadrigal@duck.com>
2023-07-02 15:31:25 -0700

    feat: Add code to make tests pass for exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+  let newArray = [];
 
+  for(element of args) {
+    if(array.includes(element)) {
+      array.splice(array.indexOf(element), 1);
+    }
+    newArray = array
+  }
+  return newArray;
 };
 
 // Do not edit below this line
